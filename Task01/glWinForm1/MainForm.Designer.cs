718bfce7169e228567faef44ceaab06b241e2ad6
DavydenkoKostya <k.i.davydenko@student.khai.edu>
2024-10-23 10:39:57 +0200

    Add files via upload

---
+++ Task01/glWinForm1/MainForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace glWinForm1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.renderControl1 = new glWinForm1.RenderControl();
+            this.SuspendLayout();
+            // 
+            // renderControl1
+            // 
+            this.renderControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.renderControl1.AutoSize = true;
+            this.renderControl1.BackColor = System.Drawing.Color.SlateGray;
+            this.renderControl1.ForeColor = System.Drawing.Color.White;
+            this.renderControl1.Location = new System.Drawing.Point(12, 12);
+            this.renderControl1.Name = "renderControl1";
+            this.renderControl1.Size = new System.Drawing.Size(500, 500);
+            this.renderControl1.TabIndex = 0;
+            this.renderControl1.TextCodePage = 1251;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 521);
+            this.Controls.Add(this.renderControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RenderControl renderControl1;
+    }
+}
+
